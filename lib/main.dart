ab10a7fd59f2ee238534a1dc23301c6d6ecd4ac1
shivanigu <shivanig.ug20.ece@nitp.ac.in>
2021-11-17 21:07:03 +0530

    controlled overflow

--- lib/main.dart
+++ lib/main.dart
@@ -58,148 +58,150 @@ TextEditingController sgstController = TextEditingController();
         ),
         body:  Padding(
             padding: EdgeInsets.all(10.0),
-            child: Column(
-              children: [
-                Text('Cost Amount',
-                style: TextStyle(
-                  fontSize: 15.0,
-                  fontWeight: FontWeight.bold,
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Text('Cost Amount',
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold,
+                    
+                  ),
+                  ),
+                  SizedBox(height: 8.0,
+                  ),
+                  TextField(
+                    keyboardType: TextInputType.number,
+                    controller: costController,
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      hintText: 'Enter the Cost Amount of the Product',
+                      filled: true,
+                      fillColor: Colors.red[100],
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                        borderSide: BorderSide.none,
+                      )
+                    ),
+                  ),
+                  SizedBox(height: 15.0,),
+                Text('Dicount',
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold,
+                    
+                  ),
+                  ),
+                  SizedBox(height: 8.0,
+                  ),
+                  TextField(
+                    keyboardType: TextInputType.number,
+                    controller: discountController,
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      hintText: 'Enter the Dicount % on Product',
+                      filled: true,
+                      fillColor: Colors.red[100],
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                        borderSide: BorderSide.none,
+                      )
+                    ),
+                  ),
+                  SizedBox(height: 15.0,),
                   
-                ),
-                ),
-                SizedBox(height: 8.0,
-                ),
-                TextField(
-                  keyboardType: TextInputType.number,
-                  controller: costController,
-                  textAlign: TextAlign.center,
-                  decoration: InputDecoration(
-                    hintText: 'Enter the Cost Amount of the Product',
-                    filled: true,
-                    fillColor: Colors.red[100],
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                      borderSide: BorderSide.none,
-                    )
+                   Text('CGST%',
+                       style: TextStyle(
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                   SizedBox(height: 8.0,
+                  ),
+                  TextField(
+                    keyboardType: TextInputType.number,
+                    controller: cgstController,
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      hintText: 'Enter the rate of CGST',
+                      filled: true,
+                      fillColor: Colors.red[100],
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                        borderSide: BorderSide.none,
+                      )
+                    ),
+                  ),
+                  SizedBox(height: 15.0,),
+                  Text('SGST%',
+                       style: TextStyle(
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                   SizedBox(height: 8.0,
+                  ),
+                  TextField(
+                    keyboardType: TextInputType.number,
+                    controller: sgstController,
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      hintText: 'Enter the rate of SGST',
+                      filled: true,
+                      fillColor: Colors.red[100],
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                        borderSide: BorderSide.none,
+                      )
+                    ),
+                  ),
+                SizedBox(height: 15.0,),
+                Container(
+                  width: double.infinity,
+                  height: 50.0,
+                  child: FlatButton(onPressed: () {
+                    setState(() {
+                      double cost = double.parse(costController.value.text);
+                    double discount = double.parse(discountController.value.text);
+                    double cgst = double.parse(cgstController.value.text);
+                    double sgst = double.parse(sgstController.value.text);
+                    calculate_payable_amount(cost, discount, cgst, sgst);
+                    });
+                    
+                  },
+                  color: Colors.red[800],
+                   child: Text('Calculate',
+                   style: TextStyle(
+                     color: Colors.white,
+            
+                   ),),
                   ),
                 ),
                 SizedBox(height: 15.0,),
-              Text('Dicount',
-                style: TextStyle(
-                  fontSize: 15.0,
-                  fontWeight: FontWeight.bold,
-                  
-                ),
-                ),
-                SizedBox(height: 8.0,
-                ),
-                TextField(
-                  keyboardType: TextInputType.number,
-                  controller: discountController,
+                Container(
+                  width: double.infinity,
+                  child: Text('Final Payable prize is : ',
                   textAlign: TextAlign.center,
-                  decoration: InputDecoration(
-                    hintText: 'Enter the Dicount % on Product',
-                    filled: true,
-                    fillColor: Colors.red[100],
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                      borderSide: BorderSide.none,
-                    )
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold
+                  ),
                   ),
                 ),
-                SizedBox(height: 15.0,),
+                 SizedBox(height: 20.0,),
+                Container(
+                  width: double.infinity,
+                  child: Text('$result',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold
+                  ),
+                  ),
+                ),
+                  ],
                 
-                 Text('CGST%',
-                     style: TextStyle(
-                  fontSize: 15.0,
-                  fontWeight: FontWeight.bold,
-                  ),
-                ),
-                 SizedBox(height: 8.0,
-                ),
-                TextField(
-                  keyboardType: TextInputType.number,
-                  controller: cgstController,
-                  textAlign: TextAlign.center,
-                  decoration: InputDecoration(
-                    hintText: 'Enter the rate of CGST',
-                    filled: true,
-                    fillColor: Colors.red[100],
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                      borderSide: BorderSide.none,
-                    )
-                  ),
-                ),
-                SizedBox(height: 15.0,),
-                Text('SGST%',
-                     style: TextStyle(
-                  fontSize: 15.0,
-                  fontWeight: FontWeight.bold,
-                  ),
-                ),
-                 SizedBox(height: 8.0,
-                ),
-                TextField(
-                  keyboardType: TextInputType.number,
-                  controller: sgstController,
-                  textAlign: TextAlign.center,
-                  decoration: InputDecoration(
-                    hintText: 'Enter the rate of SGST',
-                    filled: true,
-                    fillColor: Colors.red[100],
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                      borderSide: BorderSide.none,
-                    )
-                  ),
-                ),
-              SizedBox(height: 15.0,),
-              Container(
-                width: double.infinity,
-                height: 50.0,
-                child: FlatButton(onPressed: () {
-                  setState(() {
-                    double cost = double.parse(costController.value.text);
-                  double discount = double.parse(discountController.value.text);
-                  double cgst = double.parse(cgstController.value.text);
-                  double sgst = double.parse(sgstController.value.text);
-                  calculate_payable_amount(cost, discount, cgst, sgst);
-                  });
-                  
-                },
-                color: Colors.red[800],
-                 child: Text('Calculate',
-                 style: TextStyle(
-                   color: Colors.white,
-
-                 ),),
-                ),
               ),
-              SizedBox(height: 15.0,),
-              Container(
-                width: double.infinity,
-                child: Text('Final Payable prize is : ',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold
-                ),
-                ),
-              ),
-               SizedBox(height: 20.0,),
-              Container(
-                width: double.infinity,
-                child: Text('$result',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 25.0,
-                  fontWeight: FontWeight.bold
-                ),
-                ),
-              ),
-                ],
-              
             ),
             )
 
